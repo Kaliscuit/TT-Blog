306a39984c21f3ed35882dec88bbe5476a21d837
xiaozhui <yuhanjian@yahoo.com>
2013-05-18 09:49:25 +0800

    /

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,8 +2,8 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <script src="assets/js/jquery-1.9.1.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="/assets/js/jquery-1.9.1.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 </head>
 <body>
