05d871b49ee4d4fdcf9685423d3344d4f731e356
xiaozhui <yuhanjian@yahoo.com>
2013-05-18 09:46:36 +0800

    include js css

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="assets/js/jquery-1.9.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
--- application/views/user_login.php
+++ application/views/user_login.php
@@ -1,12 +1,22 @@
-<html>
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
-<form action="http://blog.daimazhimei.com/index.php/login/process" method="post">
-    <input name="username" type="text" />
-    <input name="password" type="password" />
-    <input type="submit" value="Submit">
+<form class="form-horizontal" action="/login/process" method="post">
+    <div class="control-group">
+        <label class="control-label" for="inputUsername">Email</label>
+        <div class="controls">
+            <input type="text" id="inputUsername" placeholder="Username" name="username">
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="inputPassword">Password</label>
+        <div class="controls">
+            <input type="password" id="inputPassword" placeholder="Password" name="password">
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="controls">
+            <label class="checkbox">
+                <input type="checkbox"> Remember me
+            </label>
+            <button type="submit" class="btn">Sign in</button>
+        </div>
+    </div>
 </form>
-</body>
-</html>
